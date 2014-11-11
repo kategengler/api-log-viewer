fb1d4380cb091a77a72c7048d803dcb3033f803e
Katie Gengler <katie@kmg.io>
2014-11-11 13:20:54 -0500

    Don't bind the filters to inputs

--- app/controllers/events.js
+++ app/controllers/events.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.ArrayController.extend({
   queryParams: ['email'],
   email: null,
+  emailFilter: Ember.computed.oneWay('email'),
   actions: {
     reset: function(){
       this.set('email', null);
